6b2d24b2ea7c930060e34b801861483db163987a
adam <adam.lagssaibi@imt-atlantique.net>
2023-10-11 15:40:09 +0200

    place added

---
+++ src/Place.java
@@ -0,0 +1,26 @@
+
+public class Place {
+	private int nbre_jetons;
+
+	public Place(int nbre_jetons) {
+		this.nbre_jetons=nbre_jetons;
+	}
+	public int getNbreJetons() {
+		return nbre_jetons;
+		
+	}
+	public void setNbreJetons(int Jetons){
+		this.nbre_jetons=Jetons;
+		
+	}
+	public void rmNbreJetons(int rm) {
+		this.nbre_jetons-=rm;
+		
+	}
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

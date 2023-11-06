c88a676a3d3e91c0a8c7a68dc085ab133b724d69
ezzakri-anas <ezzakrianas65@gmaiL.com>
2023-11-06 21:46:12 +0100

    Added tests

---
+++ Test/PlaceTest.java
@@ -0,0 +1,31 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlaceTest {
+
+	@Test
+	void testPlace() {
+		assertNotNull(new Place(5));
+	}
+
+	@Test
+	void testGetNbreJetons() {
+		Place p = new Place(10);
+		assertEquals(p.getNbreJetons(), 10);
+	}
+
+	@Test
+	void testSetNbreJetons() {
+		Place p = new Place(10);
+		p.setNbreJetons(7);
+		assertEquals(p.getNbreJetons(), 7);
+	}
+
+	@Test
+	void testRmNbreJetons() {
+		Place p = new Place(10);
+		p.rmNbreJetons(7);
+		assertEquals(p.getNbreJetons(), 10 - 7);
+	}
+}

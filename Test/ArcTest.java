ecaabb90be3e33d0ff39fb554f8aa3411a7ff20e
adam <adam.lagssaibi@imt-atlantique.net>
2023-11-06 22:20:08 +0100

    update

--- Test/ArcTest.java
+++ Test/ArcTest.java
@@ -2,6 +2,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import petrinet.Arc;
+import petrinet.Place;
+import petrinet.Transition;
+
 class ArcTest {
 
 	@Test
